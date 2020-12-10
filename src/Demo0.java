1a2a40c2a28877e2a711d753fad7fbaa45a32119
Sawyer-ghoti <1326259342@qq.com>
2020-12-10 22:18:32 +0800

    Fifth commit

--- src/Demo0.java
+++ src/Demo0.java
@@ -1,6 +1,6 @@
 public class Demo0 {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println("Horray!");
+        System.out.println(2);
+        System.out.println("Changed");
     }
 }
